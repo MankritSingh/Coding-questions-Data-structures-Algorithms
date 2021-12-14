13a78ae7ee6882c31fb6edfc7190ad8421e2c696
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2021-12-14 16:15:45 +0530

    Update prefix sum (basic of dp).cpp

--- dynamic programming/prefix sum (basic of dp).cpp
+++ dynamic programming/prefix sum (basic of dp).cpp
@@ -6,7 +6,7 @@
    O(n) where n is the length of the array.For say m number of queries the time complecity comes out to be O(n*m).Here if the value of m is big we will encounter a problem
    with the time taken to solve the problem.
    
-   Prefix sum approach:We will find the prefix sum of the given array.First we make an array of the sam size,then we find the consective sums as we go on.
+   Prefix sum approach:We will find the prefix sum of the given array.First we make an array of the same size,then we find the consective sums as we go on.
    Eg [1,3,4,5] the array we make will be 
    1)[1]            
    2)[1,4]              The sum of 1 and 3
@@ -14,7 +14,7 @@
    4)[1,4,8,13]         The sum of 1,3,4and 5
   We can easily find the sum of range starting from 0 by just seeing the array element.eg sum[0,3]=arr[3] and so on.
   For other queries we simply use the logic like sum[0,6]=sum[0,2]+sum[2,6] therefore sum[2,6]=sum[0,6]-sum[0,2]
-                                                                                      sum[2,6]=dp[6]-dp[2-10]
+                                                                                      sum[2,6]=dp[6]-dp[2-1]
 */
 #include<iostream>
 using namespace std;
