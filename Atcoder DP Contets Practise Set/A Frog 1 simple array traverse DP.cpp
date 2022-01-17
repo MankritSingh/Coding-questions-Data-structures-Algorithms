32c29f25273265744f3bc05361eb569da781f282
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-01-17 18:25:17 +0530

    Update A Frog 1 simple array traverse DP.cpp

--- Atcoder DP Contets Practise Set/A Frog 1 simple array traverse DP.cpp
+++ Atcoder DP Contets Practise Set/A Frog 1 simple array traverse DP.cpp
@@ -4,10 +4,12 @@ video link:https://www.youtube.com/watch?v=mnuBvHbMNJM
 
 I have solved this question using iterative approach too.And it works really well
 
-Analysis:We should try to see the independent problems that exist in the whole problem.For eg here we think that the cost to arrive at the final rock must be what?
+Analysis:We should try to see the independent problems that exist in the whole problem.For eg here we think that the cost to arrive
+at the final rock must be what?
 we know that we can only arrive at final rock i from the rock i-1 and rock i-2.
-Therefore this tells us one important thing that if we know the min cost to arrive at the rock i-1 and i-2 then its simple to know the min cost to arrive at 
-rock i. Extending this logic we can say that min cost to reach rock i-1 is easliy calculated by knowing the cost of rock i-2 and rock-3.
+Therefore this tells us one important thing that if we know the min cost to arrive at the rock i-1 and i-2 then its simple to 
+know the min cost to arrive at rock i. Extending this logic we can say that min cost to reach rock i-1 is easliy calculated by
+knowing the cost of rock i-2 and rock-3.
 now the cost of arriving at rock 0(starting rock) is 0.This is absolute and depends on nothiing else,therefore this also servers as our base condition.
 also the cost of arriving at rock 1 and rock 2 is also only and only dependent on rock 0,therefore that can be calculated easily too.
 This logic helped us solve this problem.
