5674fceb964ec39dd359c408769eaa2bf6fd811f
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-01-25 20:00:50 +0530

    Update 3.sort string and find freq.cpp

--- Maps/3.sort string and find freq.cpp
+++ Maps/3.sort string and find freq.cpp
@@ -3,7 +3,8 @@ Question:given string arrange them lexographically and find thier frequency
 
 Analysis:Use find() function to see if the string is present or not.If present increase its value by 1 else insert element in map
 with frequency 1
-*/#include <bits/stdc++.h>
+*/
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -14,16 +15,17 @@ int main() {
     for(int i=0;i<len;i++){
         string val;
         cin>>val;
-        v.push_back(val);
+        m[val]++;   //as by default value is 0 we get freq as 1.for each occurance this increases by 1
     }
-    for(int i=0;i<len;i++){
-        auto it=m.find(v[i]);
-        if(it!=m.end()) m[v[i]]+=1;
-        else{
-            m[v[i]]=1;
-        }
-    }
+    // for(int i=0;i<len;i++){
+    //     auto it=m.find(v[i]);
+    //     if(it!=m.end()) m[v[i]]+=1;
+    //     else{
+    //         m[v[i]]=1;
+    //     }
+    // }
     for(auto it:m){
         cout<<"String:"<<it.first<<" "<<"Freq:"<<it.second<<endl;
     }
 }
+
