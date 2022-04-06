0307899266a8dcfac2252a8a87e72f006c613982
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-04-06 18:46:23 +0530

    Update and rename 01.bitwise.txt to 02.binary representation and conversion.cpp

---
+++ Binary,Bitwise and Maths/02.binary representation and conversion.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int number=100;
+	int x=0b10; //represent binary numbers like this,with 0b prefix.It will be printed as 3
+	string num="110";
+	int stringToInt=stoi(num,0,2);//covert string to number.parameter are(string name,0,base)keep base as 10 for decimal
+	num=to_string(number);//convert number to string(decimal)
+}
