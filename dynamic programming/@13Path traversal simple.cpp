31a3f435d8608841c0e687ba1929a8ce6729d5e4
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-06-20 23:31:44 +0530

    Create @13Path traversal simple.cpp

---
+++ dynamic programming/@13Path traversal simple.cpp
@@ -0,0 +1,10 @@
+/*Question link:https://www.codingninjas.com/codestudio/problems/total-unique-paths_1081470
+Question:Imagine a MxN matrix.we are at the starting point that is 0,0 .We have to reach the bottom right point.
+The only moves allowed is either down or right.Find the total number of unique ways
+
+Analysis:When we first look at the question,the thing that comes to our brain is how will we identify the unique steps?
+first we think about how we would solve the problems in general without thinking about programming.
+Then we try to think how we can convert this logic to computer logic.
+
+Solution:
+*/
