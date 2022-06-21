1c6dc853437514e8b957477c872016ba1bd8d1b4
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-06-21 11:41:51 +0530

    Update @13Path traversal simple.cpp

--- dynamic programming/@13Path traversal simple.cpp
+++ dynamic programming/@13Path traversal simple.cpp
@@ -10,6 +10,8 @@ Solution:We observe that as we have only 2 ways ways of travelling ie right and 
 most point,then we will have only 1 option avaialble that is going directly to end point by going right or down.
 Therefore we will return 1 when we are at either extreme and otherwise return travel right+travel down.
 */
+
+//recursive solution
 #include <bits/stdc++.h> 
 int s(int m,int n,vector<vector<int>>&dp){
     if(m==1 || n==1)    return 1;
@@ -21,4 +23,38 @@ int s(int m,int n,vector<vector<int>>&dp){
 int uniquePaths(int m, int n) {
     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
     return s(m,n,dp);
+}
+
+//iterative solution
+/*We think what will be the answer if there is only 1 row or only 1 column? for both the cases no matter what the other 
+field is answer is always 1 therefore we fill all row and col 1 with 1.And for arr[i][j] we get the answer by adding
+the number of ways in the above row and the numbero of ways in left column*/
+
+#include <bits/stdc++.h> 
+int uniquePaths(int m, int n) {
+	int arr[m+1][n+1]={0};
+    for(int i=1;i<m+1;i++) arr[i][1]=1;
+    for(int i=1;i<n+1;i++) arr[1][i]=1;
+    for(int i=2;i<m+1;i++){
+        for(int j=2;j<n+1;j++){
+            arr[i][j]=arr[i-1][j]+arr[i][j-1];
+        }
+    }
+    return arr[m][n];
+}
+
+//Space optimization:In this case we observe that we only need the prevoiys row to find the next element of current row
+//therefore we can reeduce the space complexity to O(n)from O(n^2)
+#include <bits/stdc++.h> 
+int uniquePaths(int m, int n) {
+    int prev[n]={0};
+    for(int i=0;i<m;i++){
+         int temp[n]={0};
+        for(int j=0;j<n;j++){
+            if(j==0) temp[j]=1;
+            else temp[j]=prev[j]+temp[j-1];
+        }
+        copy(temp,temp+n,prev);
+    }
+    return prev[n-1];
 }
