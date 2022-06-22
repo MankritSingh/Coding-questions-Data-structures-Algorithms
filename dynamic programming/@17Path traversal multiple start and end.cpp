a0ed9029a1878be89aa50a6a47f1186dd0f0fe57
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-06-22 15:03:52 +0530

    Update @17Path traversal multiple start and end.cpp

--- dynamic programming/@17Path traversal multiple start and end.cpp
+++ dynamic programming/@17Path traversal multiple start and end.cpp
@@ -1,3 +1,10 @@
+/*Question:https://www.codingninjas.com/codestudio/problems/maximum-path-sum-in-the-matrix_797998
+question:We are given a matrix of NxM. We have to travel from the first row to the last row.THe starting and path can be
+any of the element of the row.
+
+Analysis:We will use the logic we have used prevoiusly to solve this question.We will tabulate the max path possible for each
+element and then take the max of the last row.
+*/
 //iterative
 #include<bits/stdc++.h>
 int getMaxPathSum(vector<vector<int>> &matrix)
@@ -54,3 +61,24 @@ int getMaxPathSum(vector<vector<int>> &matrix)
     return ans;
 }
 //recursive
+#include<bits/stdc++.h>
+int sol(int i,int j,vector<vector<int>> &matrix,vector<vector<int>> &dp){
+    if(j<0 || j>=matrix[0].size()) return -1e6;//we use -1e6 instead of int min to avoid integer overflow
+    if(i==0)    return matrix[0][j];
+    if(dp[i][j]!=-1)    return dp[i][j];
+     int up=matrix[i][j]+sol(i-1,j,matrix,dp);
+     int left=matrix[i][j]+sol(i-1,j-1,matrix,dp);
+     int right=matrix[i][j]+sol(i-1,j+1,matrix,dp);
+     return dp[i][j]=max(up,max(left,right));
+}
+int getMaxPathSum(vector<vector<int>> &matrix)
+{
+    int n=matrix.size();
+    int m=matrix[0].size();
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    int ans=INT_MIN;
+    for(int i=0;i<m;i++){
+        ans=max(ans,sol(n-1,i,matrix,dp));
+    }
+    return ans;
+}
