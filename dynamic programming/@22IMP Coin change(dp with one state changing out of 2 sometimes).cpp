e82d89375324916d334f31e0279d0b232fb057a6
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-07-04 13:32:07 +0530

    Create @22IMP Coin change(dp with one state changing out of 2 sometimes).cpp

---
+++ dynamic programming/@22IMP Coin change(dp with one state changing out of 2 sometimes).cpp
@@ -0,0 +1,14 @@
+/*Question:https://www.codingninjas.com/codestudio/problems/minimum-elements_3843091?leftPanelTab=0
+Analysis:The question appears to be simple and is similar to the previous questions but an important difference
+here is that,When we are making a choice we have 2 options,to take,or to not take.But this is not really the case.
+As the question says,we have unlimited supply of coins,therefore we can select 1 coin,2 coin,and so on.
+Now we may think that lets take the most amount of coins possible as it will decrease the denominations but that 
+is not the case.As we see sometimes we dont need to take the max possible coins of a given denomination,
+we can instead take a few coins less,THis will now help decrease the amount of coins used in future.
+SO how do we solve this issue?We can think that for a given coin,we can run a for loop,but the better way of
+doing this is to take a single coin and keep the index same.WHat this will do is that we will take coin 1 by one
+and remain at the same index.SO we can try all the possible sequences.
+SO the most IMPORTANT thing to learn from this question is that sometimes we only change on index/parameter
+of our given dp.
+
+*/
