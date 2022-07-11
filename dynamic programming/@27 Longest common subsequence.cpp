304e47abe72d03bdf0941fb4dc00ff7e07f64c24
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2022-07-11 12:55:19 +0530

    Update @27 Longest common subsequence.cpp

--- dynamic programming/@27 Longest common subsequence.cpp
+++ dynamic programming/@27 Longest common subsequence.cpp
@@ -59,9 +59,7 @@ string shortestSupersequence(string a, string b)
     for(int i=1;i<alen+1;i++){
         for(int j=1;j<blen+1;j++){
           if(a[i-1]==b[j--1])    dp[i][j]=1+dp[i-1][j-1];
-            else{
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+            else	dp[i][j]=max(dp[i-1][j],dp[i][j-1]);                          
         }
     }
     return dp[alen][blen]
