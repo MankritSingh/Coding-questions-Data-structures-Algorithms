fbd5c004b53d0e6f73fea8fb4f75330a75bec1cf
MankritSingh <67284593+MankritSingh@users.noreply.github.com>
2023-01-20 12:54:42 +0530

    Update 02.binary representation and conversion.cpp

--- Binary,Bitwise and Maths/02.binary representation and conversion.cpp
+++ Binary,Bitwise and Maths/02.binary representation and conversion.cpp
@@ -5,5 +5,6 @@ int main(){
 	int x=0b10; //represent binary numbers like this,with 0b prefix.It will be printed as 3
 	string num="110";
 	int stringToInt=stoi(num,0,2);//covert string to number.parameter are(string name,0,base)keep base as 10 for decimal
+	int stringToll=stoll(num,0,10);//covert string to ll.
 	num=to_string(number);//convert number to string(decimal)
 }
